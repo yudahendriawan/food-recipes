9c13fb4a99e3dbe94427770fec4697905b5bfc42
yudahendriawan <yudahendriawan007@gmail.com>
2022-03-23 08:28:36 +0700

    Update : setup placeholder row layout for shimmer

--- app/src/main/java/com/yudahendriawan/foodrecipes/RecipesFragment.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/RecipesFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yudahendriawan.foodrecipes.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private var _binding : FragmentRecipesBinding? = null
+    private val binding get() = _binding as FragmentRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+
+        binding.recyclerView.showShimmer()
+
+        return binding.root
     }
 
 }
