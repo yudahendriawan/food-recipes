450cbf024b4534eb1c41895026579dd253500226
yudahendriawan <yudahendriawan007@gmail.com>
2022-11-05 21:35:42 +0700

    Update : create and design DetailActivity.kt

---
+++ app/src/main/java/com/yudahendriawan/foodrecipes/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.yudahendriawan.foodrecipes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yudahendriawan.foodrecipes.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
