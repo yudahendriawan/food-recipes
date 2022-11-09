598f43e4af84a6d9471bc7974da8d10b640d2f46
yudahendriawan <yudahendriawan007@gmail.com>
2022-11-09 22:24:19 +0700

    Update : add insert, delete, read for favorite recipes in LocalDataSource.kt

--- app/src/main/java/com/yudahendriawan/foodrecipes/data/LocalDataSource.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/data/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.yudahendriawan.foodrecipes.data
 
 import com.yudahendriawan.foodrecipes.data.database.RecipesDao
+import com.yudahendriawan.foodrecipes.data.database.entities.FavoritesEntity
 import com.yudahendriawan.foodrecipes.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,12 +10,28 @@ class LocalDataSource @Inject constructor(
     private val recipesDao: RecipesDao
 ) {
 
-    fun readDatabase(): Flow<List<RecipesEntity>> {
+    fun readRecipes(): Flow<List<RecipesEntity>> {
         return recipesDao.readRecipes()
     }
 
     suspend fun insertRecipes(recipesEntity: RecipesEntity) {
         recipesDao.insertRecipes(recipesEntity)
+    }
+
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>> {
+        return recipesDao.readFavoriteRecipes()
+    }
+
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity) {
+        recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity) {
+        recipesDao.deleteFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun deleteAllFavoriteRecipes() {
+        recipesDao.deleteAllFavoriteRecipes()
     }
 
 }
