a671d51724cc1874a6be2d9894c692bc4e436a01
yudahendriawan <yudahendriawan007@gmail.com>
2022-11-09 22:30:13 +0700

    Update : modify RecipesDatabase.kt and should add scheme for production purposes

--- app/src/main/java/com/yudahendriawan/foodrecipes/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.yudahendriawan.foodrecipes.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.yudahendriawan.foodrecipes.data.database.entities.FavoritesEntity
 import com.yudahendriawan.foodrecipes.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,
     exportSchema = false
 )
