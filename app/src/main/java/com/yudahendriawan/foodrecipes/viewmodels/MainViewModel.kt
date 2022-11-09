4fa9e4b8534824bf0481b0f2a484780c7ed0e20b
yudahendriawan <yudahendriawan007@gmail.com>
2022-11-09 22:27:53 +0700

    Update : add insert, delete, read for favorite recipes in MainViewModel.kt

--- app/src/main/java/com/yudahendriawan/foodrecipes/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/viewmodels/MainViewModel.kt
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.lifecycle.*
 import com.yudahendriawan.foodrecipes.data.Repository
+import com.yudahendriawan.foodrecipes.data.database.entities.FavoritesEntity
 import com.yudahendriawan.foodrecipes.data.database.entities.RecipesEntity
 import com.yudahendriawan.foodrecipes.models.FoodRecipe
 import com.yudahendriawan.foodrecipes.util.NetworkResult
@@ -26,10 +27,29 @@ class MainViewModel @Inject constructor(
      */
     val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readRecipes().asLiveData()
 
+    val readFavoriteRecipes: LiveData<List<FavoritesEntity>> =
+        repository.local.readFavoriteRecipes().asLiveData()
+
     private fun insertRecipes(recipesEntity: RecipesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
             repository.local.insertRecipes(recipesEntity)
         }
+
+    private fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.insertFavoriteRecipe(favoritesEntity)
+        }
+
+    private fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.deleteFavoriteRecipe(favoritesEntity)
+        }
+
+    private fun deleteAllFavoriteRecipes() =
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.deleteAllFavoriteRecipes()
+        }
+
 
     /**
      * RETROFIT
