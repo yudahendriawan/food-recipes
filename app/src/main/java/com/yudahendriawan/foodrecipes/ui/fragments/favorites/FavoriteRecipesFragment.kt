c61fa7b6444377ffb731b49a867b047e8d1c6e10
yudahendriawan <yudahendriawan007@gmail.com>
2023-04-09 11:55:03 +1000

    Section21 - Design fragment_favorite_recipes.xml

--- app/src/main/java/com/yudahendriawan/foodrecipes/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yudahendriawan.foodrecipes.R
+import com.yudahendriawan.foodrecipes.databinding.FragmentFavoriteRecipesBinding
+import com.yudahendriawan.foodrecipes.databinding.FragmentIngredientBinding
 
 class FavoriteRecipesFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding as FragmentFavoriteRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
