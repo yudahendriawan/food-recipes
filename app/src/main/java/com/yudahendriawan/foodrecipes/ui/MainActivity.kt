f9334bd7a69f689f0e00a6df4bb93e8565a3ef06
yudahendriawan <yudahendriawan007@gmail.com>
2024-01-05 14:44:57 +1000

    motion-layout and app icon

--- app/src/main/java/com/yudahendriawan/foodrecipes/ui/MainActivity.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/ui/MainActivity.kt
@@ -14,19 +14,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var navController : NavController
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setTheme(R.style.Theme_FoodRecipes)
+
         setContentView(binding.root)
 
         navController = findNavController(R.id.navHostFragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.recipesFragment, R.id.favoriteRecipesFragment, R.id.foodJokeFragment
-        ))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment, R.id.favoriteRecipesFragment, R.id.foodJokeFragment
+            )
+        )
 
         binding.bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
