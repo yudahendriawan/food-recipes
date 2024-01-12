4268f21ca599255c322851b16640552a4a6305f4
yudahendriawan <yudahendriawan007@gmail.com>
2024-01-12 19:40:40 +1000

    multi selection mode - fixed

--- app/src/main/java/com/yudahendriawan/foodrecipes/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/yudahendriawan/foodrecipes/adapters/FavoriteRecipesAdapter.kt
@@ -74,6 +74,7 @@ class FavoriteRecipesAdapter(
         when (selectedRecipes.size) {
             0 -> {
                 mActionMode.finish()
+                multiSelection = false
             }
             1 -> {
                 mActionMode.title = "${selectedRecipes.size} item selected"
@@ -118,8 +119,8 @@ class FavoriteRecipesAdapter(
                 applySelection(holder, currentRecipe)
                 true
             } else {
-                multiSelection = false
-                false
+                applySelection(holder, currentRecipe)
+                true
             }
 
         }
